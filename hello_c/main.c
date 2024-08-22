7aeabc5d1da7541f517c9b71f4a0129e30f86d41
AndrewLa <andrew.lzrv@gmail.com>
2024-08-22 09:12:47 +0700

    New task was added

---
+++ hello_c/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "library.h"
+
+int main(int argc, char** argv) {
+    int a, b, c;
+    int x, y, z;
+
+    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &x, &y, &z);
+
+    // Place your code below
+
+    // What you can't do: use any arithmetic operators, call any function from <stdio.h>
+    // What you can do: call your function, create variables, assign values to variables
+
+    return 0;
+}
