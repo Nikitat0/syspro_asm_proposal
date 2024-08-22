0afd3748a79042bf4492eea94ccec602bf2aacc9
Nikitat0 <nikitatzero@yandex.ru>
2024-08-22 16:37:58 +0700

    Add aarch64 cross-assembler & cross-compiler to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { }
+, pkgsCrossArm64 ? import <nixpkgs> { crossSystem.config = "aarch64-linux-gnu"; }
+}:
 
 pkgs.mkShell {
-    packages = with pkgs; [
-        clang-tools
-        gcc
-        gdb
-        nasm
-    ];
+  packages = with pkgs; [
+    clang-tools
+    gcc
+    gdb
+    nasm
+    qemu
+    pkgsCrossArm64.buildPackages.gcc
+  ];
 }
